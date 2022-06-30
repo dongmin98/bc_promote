b361219ee6e8f495fff0b8dd113f03ad812c6416
董敏 <13329754822@163.com>
2022-06-30 11:30:34 +0800

    first

---
+++ src/main/java/com/bc/promote/BcPromoteApplication.java
@@ -0,0 +1,13 @@
+package com.bc.promote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcPromoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BcPromoteApplication.class, args);
+    }
+
+}
