6c5f90073dd5ce80fe2212b5fbc0877d2a3cc889
董敏 <13329754822@163.com>
2022-07-01 09:24:40 +0800

    初始化配置

--- bc_promote_back/src/main/java/com/bc/promote/BcPromoteApplication.java
+++ bc_promote_back/src/main/java/com/bc/promote/BcPromoteApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BcPromoteApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BcPromoteApplication.class, args);
     }
-
 }
